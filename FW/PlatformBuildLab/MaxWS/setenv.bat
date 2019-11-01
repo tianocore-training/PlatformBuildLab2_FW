69af0321a2609c52b40abb4fdefe491ef4a1e8d8
Laurie0131 <laurie.jarlstrom@intel.com>
2019-11-01 09:45:43 -0700

    update setenv.bat for Openssl path

--- FW/PlatformBuildLab/MaxWS/setenv.bat
+++ FW/PlatformBuildLab/MaxWS/setenv.bat
@@ -1,6 +1,6 @@
 set WORKSPACE=%CD%
 set PACKAGES_PATH=%WORKSPACE%\edk2;%WORKSPACE%\edk2-platforms\Silicon\Intel;%WORKSPACE%\edk2-platforms\Platform\Intel;%WORKSPACE%\edk2-non-osi\Silicon\Intel
 set EDK_TOOLS_PATH=%WORKSPACE%\edk2\BaseTools
-path=%path%;%WORKSPACE%\openssl;%USERPROFILE%\AppData\Local\Programs\Python\Python37-32
+path=%WORKSPACE%\openssl;%USERPROFILE%\AppData\Local\Programs\Python\Python37-32;%path%
 set NASM_PREFIX=%WORKSPACE%\nasm\
 set OPENSSL_CONF=%WORKSPACE%\openssl\apps\openssl.cfg
